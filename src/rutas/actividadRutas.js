99d6861027fa1e0204627b4ddf398683dc831d72
PaoC025 <pccm025@gmail.com>
2025-03-18 23:51:26 -0400

    Rutas de las actividades actualizadas

--- src/rutas/actividadRutas.js
+++ src/rutas/actividadRutas.js
@@ -4,7 +4,15 @@ const {
     obtenerActividades,
     obtenerActividadPorId,
     actualizarActividad,
-    eliminarActividad
+    eliminarActividad,
+    obtenerActividadesPorCategoria,
+    obtenerUltimasActividades,
+    obtenerActividadesPorProyecto,
+    obtenerActividadesPorHabitoYRangoFechas,
+    obtenerHabitosSinActividades,
+    buscarActividadesPorNombre,
+    calcularTiempoPorCategoriaOProyecto,
+    obtenerActividadesAbiertas,
 } = require("../controladores/actividadControlador");
 
 const router = express.Router();
@@ -15,5 +23,12 @@ router.get("/", obtenerActividades);      // Obtener todas las actividades
 router.get("/:id", obtenerActividadPorId); // Obtener actividad por ID
 router.put("/:id", actualizarActividad);  // Actualizar actividad
 router.delete("/:id", eliminarActividad); // Eliminar actividad
-
+router.get("/usuarios/:usuarioId/categorias/:categoria", obtenerActividadesPorCategoria); //Obtener Actividades por categoría
+router.get("/usuarios/:usuarioId/ultimas-actividades", obtenerUltimasActividades); //Obtener Ultimas Actividades
+router.get("/proyectos/:proyecto/actividades", obtenerActividadesPorProyecto); //Obtener Actividades por Proyecto
+router.get("/habitos/:habitoId/actividades/:fechaInicio/:fechaFin", obtenerActividadesPorHabitoYRangoFechas); //Obtener Actividades por Habito y Rango de Fechas
+router.get("/sin-actividades", obtenerHabitosSinActividades); //Obtener Habitos sin Actividades
+router.get("/buscar/:nombre", buscarActividadesPorNombre); //Actividades por Nombre
+router.get("/tiempo/:agruparPor", calcularTiempoPorCategoriaOProyecto); //Tiempo por categoria o proyecto
+router.get("/sin-finalizar", obtenerActividadesAbiertas); //Actividades Abiertas
 module.exports = router;
